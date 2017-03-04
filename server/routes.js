e988b4430060c00d2d50ffaabf92b88968b5e689
nikhil-bhandari <nikhil.bhandari@6point9.in>
2017-03-05 00:25:12 +0530

    response now sends back the value recieved

--- server/routes.js
+++ server/routes.js
@@ -6,13 +6,13 @@ module.exports = function (app) {
     app.use(express.static(__dirname + "/../client/"));
 
     app.get("/api/pins/:pin/:output", function (req, res) {
-        if (req.param.output == "on") {
+        if (req.params.output == "on") {
             var relay = new Gpio(req.params.pin, 'out');
         } else {
             var relay = new Gpio(req.params.pin, 'in');
         }
         res
             .status(200)
-            .json({ok: true})
+            .json({ok: true, pin: req.params.pin, output: req.params.output})
     })
 };
